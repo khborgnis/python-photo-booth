1245307f26119610e46ed811eace90de63821894
Kevin Holian-Borgnis <kevin@borgnis.org>
2017-02-18 00:34:10 -0500

    Multithreading working. Updated TODO

--- photobooth.py
+++ photobooth.py
@@ -7,13 +7,12 @@ import cv2
 import Queue
 
 class PhotoBoothApp:
-    def __init__(self, video_camera, output_path):
-        self.video_camera = video_camera
+    def __init__(self, video_stream, output_path):
+        self.video_stream = video_stream
         self.output_path = output_path
 
         # Properties for multi-threading
-#        self.queue = Queue.Queue()
-#        self.run_event = True
+        self.run_event = True
 
         self.frame_video = None
 
@@ -22,81 +21,96 @@ class PhotoBoothApp:
         self.video_panel = None
 
         # Create the button to add under the video panel
-#        btnTakeImage = tk.Button(self.window, "Take a Photo!",
-#            command=self.take_a_picture)
-#	btnTakeImage.pack(side="bottom", fill="both", expand="yes", padx="15",
-#            pady="15")
+        btnTakeImage = tk.Button(self.window, text="Take a Photo!",
+            command=self.take_a_picture)
+	btnTakeImage.pack(side="bottom", fill="both", expand="yes", padx="15",
+            pady="15")
 
-        # if the panel is not None, we need to initialize it
+        self.stopEvent = threading.Event()
+        self.thread = threading.Thread(target=self.process_images, args=())
+        self.thread.start()
 
         # Set title and close window callback
         self.window.wm_title("Photo Booth App")
         self.window.wm_protocol("WM_DELETE_WINDOW", self.on_close)
 
-        self.show_image()
-        #self.queue.put(CameraTask(self.queue, self.window, self.video_camera))
 
-    def process_queue(self):
+    def process_images(self):
         while self.run_event:
-            try:
-                task = self.queue.get(block=False)
-            except Empty:
-                self.window.after(100, self.process_queue)
-            else:
-                self.after_idle(task)
+            self.show_image()
+        self.stopEvent.set()
+        self.video_stream.stop()
+        self.window.quit()
 
     def on_close(self):
         # Stop the camera and quit
         print "[INFO] Exiting the Photo Booth..."
         self.run_event = False
-        #self.video_camera.release()
-        self.window.quit()
 
     def show_image(self):
-        image = self.video_camera.capture_frame()
+        self.frame = self.video_stream.read()
+        image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+
+        # Crop the image to be square for Instagram
+        height, width = image.shape[:2]
+        x1 = (width-height)//2
+        x2 = x1+height
+        image = image[0:height, x1:x2]
+
         image = Image.fromarray(image)
         image = ImageTk.PhotoImage(image)
 
+        print "image size: %dx%d" % (image.width(), image.height())
+
+        # if the panel is not None, we need to initialize it
         if self.video_panel is None:
             self.video_panel = tk.Label(image=image)
             self.video_panel.image = image
             self.video_panel.pack(side="left", padx="10", pady="10")
+        else:
+            self.video_panel.configure(image=image)
+            self.video_panel.image = image
 
     def take_a_picture(self):
-        return None
+        print "Take a picture!"
 
-class VideoCamera(object):
-    def __init__(self):
+from cv2 import cv
+class WebcamStream:
+    def __init__(self, src=0):
         # OpenCV to capture an image from device 0
-        self.video = cv2.VideoCapture(0)
+        self.video = cv2.VideoCapture(src)
 
-    def __del__(self):
+        self.video.set(5,30)
+        self.video.set(3,1920)
+        self.video.set(4,1080)
+
+        w = self.video.get(3)
+        h = self.video.get(4)
+        print "[INFO] Starting video stream with resolution " + str(w) + "x" + str(h)
+        (self.grabbed, self.frame) = self.video.read()
+
+        self.running = True
+
+    def start(self):
+        threading.Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        while self.running:
+          (self.grabbed, self.frame) = self.video.read()
+
+    def read(self):
+        return self.frame
+
+    def stop(self):
+        self.running = False
         self.video.release()
+        
 
-    def capture_frame(self):
-        rc, image = self.video.read()
-
-        # OpenCV defaults to raw images. Do we need to conver these?
-        return image
-
-class CameraTask(threading.Thread):
-    def __init__(self, queue, app, camera):
-        threading.Thread.__init__(self)
-        self.queue = queue
-        self.camera = camera
-        self.window = app
-
-    def run(self):
-        image = self.camera.capture_frame()
-
-        if self.window.video_panel is None:
-            self.window.video_panel = tk.Label(image=image)
-            self.window.video_panel.image = image
-            self.window.video_panel.pack(side="left", padx="10", pady="10")
-        else:
-            self.window.video_panel.configure(image=image)
-            self.window.video_panel.image = image
-video_camera = VideoCamera()
+video_camera = WebcamStream(0)
+video_camera.start()
 
 pb = PhotoBoothApp(video_camera, "output")
 pb.window.mainloop()
+
+cv2.destroyAllWindows()
